9cdc4c1e9f78e1d5d171e1d8ffae4676c11a6aa5
zmasksss <zmasksss@gmail.com>
2016-04-12 15:28:23 +0900

    test

--- HelloServlet/src/com/oraclejava/HelloWorld.java
+++ HelloServlet/src/com/oraclejava/HelloWorld.java
@@ -3,10 +3,13 @@ package com.oraclejava;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class HelloWorld extends HttpServlet {
 
@@ -16,9 +19,18 @@ public class HelloWorld extends HttpServlet {
 					throws ServletException, IOException {
 
 		PrintWriter out = res.getWriter();
-
+		ServletContext sc = getServletContext();
+		String ssname = (String)sc.getAttribute("userid");
+		ServletConfig sc2 = getServletConfig();
+		String msg = sc2.getInitParameter("msg2");
+		String cMsg = sc.getInitParameter("msg");
+		String cMsg2 = sc.getInitParameter("msg2");
+		
 		out.println("<html>");
-		out.println("<body><h1>Hello World!!</h1></body>");
+		String name = req.getParameter("name1");
+		String sname = (String)req.getSession().getAttribute("userid");
+		String userid = (String)req.getAttribute("userid");
+		out.println("<body><h1>Hello "+userid+" : "+ name +" : "+ sname +":"+ssname+":"+msg+":"+cMsg+":"+cMsg2+"</h1></body>");
 		out.println("</html>");
 
 	}
--- HelloServlet/src/com/oraclejava/ReqServ.java
+++ HelloServlet/src/com/oraclejava/ReqServ.java
@@ -4,10 +4,13 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class ReqServ extends HttpServlet {
 
@@ -43,7 +46,29 @@ public class ReqServ extends HttpServlet {
 		out.println(req.getRemoteAddr());
 		out.println("</body>");
 		out.println("</html>");
-
+		
+		/* request, response 요청이 사라진다 */
+		/*
+		 * foward
+		 * ServletContext sc = getServletContext();
+		 * RequestDispatcher : 요청관리자 
+		 * RequestDispatcher rd = sc.getRequestDispatcher("/HelloWorld"); //요청을 처리할 서블릿 컨텍스트를 얻는다
+		 * RequestDispatcher rd = req.getRequestDispatcher("/HelloWorld");
+		 * rd.forward(req, resp);
+		*/
+		
+		/*session scope*/
+		HttpSession session = req.getSession();
+		session.setAttribute("userid", "oraclejava");
+		
+		/*application scope*/
+		ServletContext sc = getServletContext();
+		sc.setAttribute("userid", "oraclejava_app1");
+		
+		RequestDispatcher rd = req.getRequestDispatcher("/HelloWorld");
+		req.setAttribute("userid", "test2");
+		//resp.sendRedirect("http://www.naver.com");
+		rd.forward(req, resp);
 	}
 
 }
